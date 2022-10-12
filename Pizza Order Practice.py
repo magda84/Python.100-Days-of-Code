bd62e6e7dd53a8cc078bccf549752e9d0c8d09b0
magda84 <86167884+magda84@users.noreply.github.com>
2022-10-12 19:32:20 +0300

    Day03

---
+++ Pizza Order Practice.py
@@ -0,0 +1,29 @@
+# Don't change the code below 👇
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# Don't change the code above 👆
+
+# Write your code below this line 👇
+
+bill = 0
+pepperoni = 0
+
+if size == "S":
+    bill += 15
+    if add_pepperoni == "Y":
+        bill += 2
+elif size == "M":
+    bill += 20
+    if add_pepperoni == "Y":
+        bill += 3
+elif size == "L":
+    bill += 25
+    if add_pepperoni == "Y":
+        bill += 3
+
+if extra_cheese == "Y":
+    bill += 1
+
+print(bill)
