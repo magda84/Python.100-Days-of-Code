ca7c3972b9e6154095d459b7aa2a19ec60cb7e22
magda84 <86167884+magda84@users.noreply.github.com>
2022-10-12 12:52:04 +0300

    Day02

--- Tip Calculator.py
+++ Tip Calculator.py
@@ -4,8 +4,8 @@ tip = int(input("What percentage tip would you like to give?\n"))
 people = int(input("How many people to split the bill?\n"))
 
 tip_calc = (bill * (tip / 100))
-total_bill = (bill + tip_calc) / people
+bill_per_person = (bill + tip_calc) / people
 
-total_bill = round(total_bill, 2)
-
-print(f"Each person should pay € {total_bill}.")
+bill_per_person = round(bill_per_person, 2)
+total_amount = "{:.2f}".format(bill_per_person)
+print(f"Each person should pay € {total_amount}.")
